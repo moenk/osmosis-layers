37b86e9e8158ffa75ac7b45ac2ba5651ffb74fdc
Thomas Moenkemeier <moenk@geoclub.de>
2011-07-25 06:43:54 +0200

    doppelte einträge

--- enforcement.sql
+++ enforcement.sql
@@ -4,6 +4,7 @@ delete from geometry_columns where f_table_name='enforcement';
 drop index if exists idx_enforcement;
 drop table if exists enforcement;
 
+-- erstmal die devices aus relationen in neue tabelle
 create table enforcement as 
 select n.id, d.enforcement, d.maxspeed, n.geom from (
   select r.id, m.member_id, r.tags->'enforcement' as enforcement, r.tags->'maxspeed' as maxspeed 
@@ -17,12 +18,25 @@ select id, tags->'highway' as enforcement, tags->'maxspeed' as maxspeed, geom
 from nodes as n
 where n.tags->'highway'='speed_camera';
 
+-- und doppelte wieder raus
+delete from enforcement 
+where id in (
+  select n.id from enforcement as lula 
+  inner join enforcement as n on lula.id=n.id 
+  and lula.enforcement='maxspeed' 
+  and n.enforcement='speed_camera') 
+and enforcement='speed_camera';
+
+-- korrektur des alten tags speed_camera
+update enforcement set enforcement='maxspeed' where enforcement='speed_camera';
+update enforcement set maxspeed='0' where maxspeed is null;
+
+-- geometrie anlegen
 insert into geometry_columns 
 (f_table_catalog, f_table_schema, f_table_name, f_geometry_column, coord_dimension, srid, "type") 
 values 
 (' ', 'public', 'enforcement', 'geom', 2, 4326, 'POINT');
 
-update enforcement set maxspeed='0' where maxspeed is null;
 
 create index idx_enforcement on enforcement using gist(geom);
 
